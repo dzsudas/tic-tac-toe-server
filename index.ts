3a9e4c2ecad2ca7267e255bdc2948e98991c4340
Dzianis Sudas <dzianis.sudas@gmail.com>
2020-10-21 09:58:11 -0400

    v2 server implementation

--- index.ts
+++ index.ts
@@ -1,80 +1,70 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import GameDao from "./GameDao";
 import process from "process";
+import {Game} from "./Game";
 const app = express();
 
 app.use(cors())
 app.use(bodyParser.json())
 
+const games = {};
+
 app.get('/game/:gameId', function (req, resp) {
-    const game = GameDao.getGame(req.params.gameId);
+    const game = games[req.params.gameId];
 
-    if (game) {
-        resp.json({
-            state: game.state,
-            isFinished: game.isGameFinished()
-        });
-    } else {
+    if (!game) {
         resp.status(404).send();
+        return;
     }
-})
 
-app.post('/game/:gameId/checkWon', function (req, resp) {
-    const id = req.params.gameId;
-    const game = GameDao.getGame(id);
-    const userId = req.body.userId;
-
-    if (game) {
-        resp.json({
-            player_won: game.checkUserWon(userId)
-        })
-    } else {
-        resp.status(404).send();
-    }
+    resp.json(game.serialize())
 })
 
 app.post('/game/create', function (req, resp) {
-    const game = GameDao.createGame();
+    const game = new Game();
 
-    resp.json({
-        id: game.id,
-        userId: game.user1Id
-    });
+    games[game.gameId] = game;
+
+    resp.status(201).send(game.gameId);
 });
 
-app.post('/game/:gameId/join', function (req, resp) {
-    const id = req.params.gameId;
-    const game = GameDao.getGame(id);
+app.post('/game/join', function (req, resp) {
+    const game = games[req.body.gameId];
 
-    if (game) {
-        resp.json({
-            userId: game.joinUser()
-        })
-    } else {
-        resp.status(404).json({
-            status: 'Not Found'
-        });
-    }
-})
-
-app.post('/game/:gameId/move', function (req, resp) {
-    const id = req.params.gameId;
-
-    const userId = req.body.userId;
-    const [x, y] = req.body.move;
-
-    const game = GameDao.getGame(id);
-
-    if (game) {
-        resp.json({
-            success: game.move(userId, x, y)
-        });
-    } else {
+    if (!game) {
         resp.status(404).send();
+        return;
     }
+
+    const playerId = game.join(req.body.team)
+
+    if (!playerId) {
+        resp.status(400).send();
+        return;
+    }
+
+    resp.send(playerId);
 })
+
+app.post('/game/move', function (req, resp) {
+    const game = games[req.body.gameId];
+    const playerId = req.body.playerId;
+    const X = parseInt(req.body.X, 10);
+    const Y = parseInt(req.body.Y, 10);
+
+    if (!game) {
+        resp.status(404).send();
+        return;
+    }
+
+    if (!game.isValidPlayer(playerId)) {
+        resp.status(403).send();
+        return;
+    }
+
+    resp.send(game.move(playerId, X, Y));
+});
 
 const server = app.listen(process.env.PORT || '8888', () => {
     const {port} = server.address() as any;
